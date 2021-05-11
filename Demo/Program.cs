2d96d43aaccc2f61de7e1dffb2dc08be402bee3c
Phoebe <jia.520@outlook.com>
2021-05-11 07:14:08 -0700

    added action demo

---
+++ Demo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using RRModels;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is an Action Demo");
+            RunActionDemo();
+        }
+
+        static void RunActionDemo(){
+            //get some dummy data from Restaurant Review
+            List<Restaurant> Restaurants = new List<Restaurant>() 
+            {
+                new Restaurant("sushi land", "Seattle", "WA"),
+                new Restaurant("Homegrown", "Bellevue", "WA")
+            };
+
+            Restaurants[0].Reviews = new List<Review>
+            {
+                new Review{
+                    Rating = 5,
+                    Description = "best sushi"
+                },
+                new Review{
+                    Rating = 4,
+                    Description = "good sushi"
+                }
+            };
+
+            //Leverage Action to create a print method as a parameter which used to 
+            // pass in to List<T>.ForEach method that will print each item on console
+            Action<Object> print = obj => Console.WriteLine(obj.ToString());
+
+            //print a list of retuarants
+            Restaurants.ForEach(print);
+            //print a list of reviews of Sushi Land
+            Restaurants[0].Reviews.ForEach(print);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Functions-Actions-Predicates-Code
 Demo Code for the Functions/Actions/Predicates presentation
+
+## Action Demo
+Execute `SeeActionDemo();` on **Program.cs** and play around with the Action Demo
